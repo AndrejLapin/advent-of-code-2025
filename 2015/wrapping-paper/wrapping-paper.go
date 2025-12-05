3595d9f17d44825e682538e6ddab14bb3be8a2db
Andrej Lapin <lapin.andrej1@gmail.com>
2025-12-05 16:44:03 +0000

    2015 day 2-1

---
+++ 2015/wrapping-paper/wrapping-paper.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fileName := flag.String("input", "input.txt", "input file")
+	flag.Parse()
+
+	file, err := os.Open(*fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	wrappingPaperTotal := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) == 0 {
+			break
+		}
+		dimensionsStrings := strings.Split(line, "x")
+		dimensions := [3]int{}
+		for index, value := range dimensionsStrings {
+			dimensions[index], _ = strconv.Atoi(value)
+		}
+		smallestSide := math.MaxInt
+		sides := [3]int{} // i don't think we even need this
+		for index, _ := range sides {
+			sides[index] = dimensions[index] * dimensions[(index+1)%len(dimensions)]
+			if sides[index] < smallestSide {
+				smallestSide = sides[index]
+			}
+			wrappingPaperTotal += sides[index] * 2
+		}
+		wrappingPaperTotal += smallestSide
+	}
+	fmt.Println(wrappingPaperTotal)
+}
